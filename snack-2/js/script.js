31936f822c1c3ab2a2013cb899b2d08c51786779
UmbertoSerino <serumberto@gmail.com>
2023-11-14 12:17:29 +0100

    created div with id

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -1,2 +1,7 @@
 // Scrivo in pagina i primi 6 numeri, scrivendo i numeri pari verdi. BN: numeri dispari gialli
-console.log("write-number")
+const div = document.querySelector ("write.number")
+
+for (let i = 1; i <= 6; i++){
+    const element = 
+
+}
