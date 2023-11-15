0c591f1a4b91f70f43a4362a60959a7d8a1d2841
UmbertoSerino <serumberto@gmail.com>
2023-11-15 12:24:58 +0100

    complete snack 4

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -1,7 +1,9 @@
+// Ciclare per i primi 100 numeri e inserire in un array esclusivamente i numeri divisibili per 7 o per 8.
 
+const newArray = [];
 
-for (let index = 0; index <= 100; index++) {
-    const numberDivisible = index 
-    console.log(numberDivisible);
-    if 
+for (let index = 1; index <= 100; index++) {
+    if (index % 7 === 0 || index % 8 === 0)
+    newArray.push(index)
 }
+console.log(newArray)
