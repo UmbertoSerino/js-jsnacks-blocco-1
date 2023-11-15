5390963f10a3a14aa326fccb5b495addcfe5afa4
UmbertoSerino <serumberto@gmail.com>
2023-11-15 12:05:08 +0100

    created cycle

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -1,8 +1,9 @@
-// Scrivo in pagina i primi 6 numeri, scrivendo i numeri pari verdi. BN: numeri dispari gialli
-const div = document.querySelector ("write.number")
-console.log("write-number")
+ Scrivo in pagina i primi 6 numeri, scrivendo i numeri pari verdi. BN: numeri dispari gialli
+ const wrapperElement = document.querySelector ("write.number")
+ console.log("write-number")
 
-for (let i = 1; i <= 6; i++){
-    const element = (<span class="write-number"> + i</span>);
+ for (let i = 1; i <= 6; i++){
+     wrapperElement.innerHTML += "<p> Ciao</p>";
+     console.log(i)
 
 }
---
+++ snack-4/js/script.js
@@ -0,0 +1,5 @@
+
+
+for (let index = 0; index <= 100; index++) {
+    console.log(index)    
+}
