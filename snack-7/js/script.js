9161fea218f0044e418bf7ac6900c61fe4826444
UmbertoSerino <serumberto@gmail.com>
2023-11-16 15:59:25 +0100

    Chiedi per 6 volte all'utente di inserire un numero, se è dispari inseriscilo nell'array.

--- snack-7/js/script.js
+++ snack-7/js/script.js
@@ -1,12 +1,17 @@
-const nameList = ["LeonardoDiCaprio", "TobeyMaguire", "CarreyMullingan"];
+// Crea un array vuoto. Chiedi per 6 volte all'utente di inserire un numero, se è dispari inseriscilo nell'array.
 
-const putYourName = prompt("inserisci il nome dell'attore")
+// creiamo un array vuoto
+const insertList = [];
 
-let people = false;
-
-for (let i = 0; i < nameList.length; i++){
-    if (nameList [i] === putYourName) {
-        people = true;
-        alert()
+// ci creiamo un ciclo dove va ripetuta l'azione per 6 volte
+for (let index = 0; index < 6; index++) {
+    // creiamo una variabile e chiediamo al cliente di inserire dei numeri
+    let userNumber = parseInt(prompt("Inserisci il tuo numero"));
+    // se il numero inserito dal cliente è divisibile per 2 e se il resto è diverso da 0 allora è un numero dispari
+    if (userNumber % 2 !== 0) {
+        // se il numero è dispari usiamo la funzione insertList.push(nome dell'array) per inserirlo nell'array vuoto
+        insertList.push(userNumber);
     }
 }
+// infine controlliamo in console il numero degli array inseriti in lista
+console.log("Numeri dispari inseriti:", insertList);
